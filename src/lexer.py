c15bd177f9f210c864d76d6180672ec357905815
Captainexpo-1 <captainexpo@gmail.com>
2024-12-07 20:01:44 -0500

    REMOVED THE BAD TOKEN ';' IT SUCKS

--- src/lexer.py
+++ src/lexer.py
@@ -1,44 +1,43 @@
 import re
-from enum import Enum
+from enum import Enum, auto
 class TokenType(Enum):
-    SKIP = 1
-    NUMBER = 2
-    ASSIGN = 3
-    LPAREN = 4
-    RPAREN = 5
-    LBRACE = 6
-    RBRACE = 7
-    SEMICOLON = 8
-    MUL = 9
-    DIV = 10
-    PLUS = 11
-    MINUS = 12
-    NEWLINE = 13
-    COMMA = 14
-    DOT = 15
-    LT = 16
-    GT = 17
-    LE = 18
-    GE = 19
-    EQ = 20
-    NE = 21
-    AND = 22
-    OR = 23
-    NOT = 24
-    IF = 25
-    ELSE = 26
-    TRUE = 27
-    FALSE = 28
-    NULL = 29
-    ID = 30
-    STRING = 31
-    MISMATCH = 32
-    EOF = 33
-    OF = 34
-    VARIABLE_NAME = 35
-    AMPERSAND = 36
-    LBRACK = 37
-    RBRACK = 38
+    SKIP = auto()
+    NUMBER = auto()
+    ASSIGN = auto()
+    LPAREN = auto()
+    RPAREN = auto()
+    LBRACE = auto()
+    RBRACE = auto()
+    MUL = auto()
+    DIV = auto()
+    PLUS = auto()
+    MINUS = auto()
+    NEWLINE = auto()
+    COMMA = auto()
+    DOT = auto()
+    LT = auto()
+    GT = auto()
+    LE = auto()
+    GE = auto()
+    EQ = auto()
+    NE = auto()
+    AND = auto()
+    OR = auto()
+    NOT = auto()
+    IF = auto()
+    ELSE = auto()
+    TRUE = auto()
+    FALSE = auto()
+    NULL = auto()
+    ID = auto()
+    STRING = auto()
+    MISMATCH = auto()
+    EOF = auto()
+    OF = auto()
+    VARIABLE_NAME = auto()
+    AMPERSAND = auto()
+    LBRACK = auto()
+    RBRACK = auto()
 
 # Define token specifications
 token_specification = [(i[0], re.compile(i[1])) for i in [
@@ -51,7 +50,6 @@ token_specification = [(i[0], re.compile(i[1])) for i in [
     (TokenType.RBRACE,   r'\}'),           # Right brace
     (TokenType.LBRACK,   r'\['),           # Left brack
     (TokenType.RBRACK,   r'\]'),           # Right brack
-    (TokenType.SEMICOLON, r';'),           # Statement terminator
     (TokenType.MUL,      r'\*'),           # Multiplication operator
     (TokenType.DIV,      r'/'),            # Division operator
     (TokenType.PLUS,     r'\+'),           # Addition operator
