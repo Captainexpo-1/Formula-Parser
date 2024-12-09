a378d86232c3d5c46dbe92c22a5fcc857e5286ae
Captainexpo-1 <captainexpo@gmail.com>
2024-12-08 19:24:29 -0500

    small

--- src/formula.py
+++ src/formula.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from lexer import Token, TokenType, lex
 from parse import ASTNode, Parser
 from eval import evaluate
@@ -24,22 +23,3 @@ class Formula:
             return evaluate(self.ast, row)
         except:
             return None
-=======
-from lexer import Token, TokenType, lex
-from parse import ASTNode, Parser
-from eval import evaluate
-
-class Formula:
-    def __init__(self, code: str):
-        self.code = code
-        self.parser = Parser()
-        self.ast: ASTNode = self.parse()
-    
-    def parse(self):
-        tokens = lex(self.code)
-        print(tokens)
-        return self.parser.parse(tokens)
-    
-    def evaluate(self, row: dict[str, any]) -> any:
-        return evaluate(self.ast, row)
->>>>>>> c0e54d49ad7f0c763749c1f9c7a386b9415be23a
